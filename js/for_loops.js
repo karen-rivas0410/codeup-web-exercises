302f0095b188dc04f6ee68967e628443f899d22f
karen-rivas0410 <karen-rivas0410@gmail.com>
2018-10-10 09:13:20 -0500

    exercise for loops

---
+++ js/for_loops.js
@@ -0,0 +1,57 @@
+"use strict";
+
+// function showMultiplicationTable(num)
+//
+//     for (var i = 1; i < 11; i++) {
+//         console.log(7 * i);
+//     }
+//
+//
+// console.log(showMultiplicationTable);
+
+
+// 123 is odd
+// 80 is even
+// 24 is even
+// 199 is odd
+
+// function isEven(num) {
+//     return num % 2 === 0;
+// }
+//
+//
+// var allCones = Math.floor(Math.random() * 100) + 100; //make sure you have the variable inside the for//
+//
+// for (var i = allCones; i < 200; i++) {
+//
+//     if (i % 2 !== 0) {
+//         console.log(i + ' is odd');
+//         break;
+//     } //can have an else to simplify it///
+//     if (i % 2 === 0) {
+//         console.log(i + ' is even');
+//         break;
+//     }
+// }console.log(i);
+
+    // var output = "";
+    // for (var i = 1; i <= 9; i++) {
+    //     output = "";
+    //     for (var j = 1; j <= i; j++) {
+    //         output += i;
+    //     }
+    //     console.log(output);
+    //     output = "";
+    // }
+
+
+
+
+// for (var i = 100; i >= 5; i -= 5) {
+//     console.log(i);
+// }
+
+
+
+
+
