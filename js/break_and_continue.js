d441bd10f7ff14adeca31c409c464d8b563b9d25
karen-rivas0410 <karen-rivas0410@gmail.com>
2018-10-10 09:19:23 -0500

    added solution to last example

---
+++ js/break_and_continue.js
@@ -0,0 +1,25 @@
+"use strict";
+
+// Prompt the user for an odd number between 1 and 50. Use a loop and a break statement to continue prompting the user if they enter invalid input.
+// Use a loop and the continue statement to output all the odd numbers between 1 and 50, except for the number the user entered.
+
+var oddNumber = prompt("Choose an odd number between 1 and 50!"); ///need to use is not a number and parsefloat it needs to be inside for or function  missing
+//the first part to be in a do while loop// has to be promted AT LEAST ONCE//
+
+for (var i = oddNumber; i < 50; i++) {
+    if (oddNumber % 2 == 0) {
+        oddNumber = prompt('Invalid Input');
+    }
+}
+// while (true) {
+//    var i = oddNumber;
+//     if (i % 2 === 0) {
+//         i = prompt("Invalid Input");
+//
+    }
+    for (var i = 0; i < 50; i+=2){
+        if (i === oddNumber); {
+            console.log("Yikes! Skipping Number: " + oddNumber);
+            continue;
+        }
+    }
