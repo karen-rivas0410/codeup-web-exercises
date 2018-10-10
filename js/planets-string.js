c7b825a72478c68553b20b9e4cbbe6a6d8d3e0c8
karen-rivas0410 <karen-rivas0410@gmail.com>
2018-10-10 16:53:39 -0500

    added last solution to the bonus question

---
+++ js/planets-string.js
@@ -0,0 +1,48 @@
+(function(){
+    "use strict";
+
+
+    // var planetsString = "Mercury|Venus|Earth|Mars|Jupiter|Saturn|Uranus|Neptune";
+    // var planetsArray = planetsString.split("|");
+
+    /**
+     * TODO:
+     * Convert planetsString to an array, and save it in a variable named
+     * planetsArray.
+     * console.log planetsArray to check your work
+     */
+
+    // console.log(planetsArray);
+
+
+    /**
+     * TODO:
+     * Create a string with <br> tags between each planet. console.log() your
+     * results. Why might this be useful?
+     */
+
+    // planetsArray.join('<br>');
+    // console.log(planetsArray);
+
+
+
+    /**
+     * BONUS:
+     * Create another string that would display your planets in an undordered
+     * list. You will need an opening AND closing <ul> tags around the entire
+     * string, and <li> tags around each planet.
+     */
+
+
+    // var columnNames = array.shift();
+    // columnNames = columnNames.split(", ");
+    // var htmlString = "<table><tr><th>" + columnNames[0] + "</th><th>" + columnNames[1] + "</th></tr>";
+    // array.forEach(function(line) {
+    //     var pieces = line.split(",");
+    //     htmlString += "<tr>"
+    //         + "<td>" + pieces[0] + "</td>"
+    //         + "<td>" + pieces[1] + "</td></tr>";
+
+})();
+
+
