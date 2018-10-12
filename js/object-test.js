b4aceca9fa07ec8f435d5a075d54ea41668173e0
karen-rivas0410 <karen-rivas0410@gmail.com>
2018-10-12 08:18:11 -0500

    added exercise for object;how to create an object

---
+++ js/object-test.js
@@ -0,0 +1,26 @@
+"use strict";
+
+var blogpost = {
+    author: "Karen Rivas",
+    datePosted: '01/10/89',
+    averageRating: 4,
+    title: "Hello World",
+    noOfShares: 1000000000,
+    noOfViews: 1000000000000,
+    isFlagged: true,
+    lastUpdated: "yesterday",
+    content: "Hello, I'm Karen!!!!!"
+};
+
+//Or you can write it the ancient way//
+
+var blogpost = {};
+        blogpost.author = 'Karen Rivas';
+        blogpost.datePosted = '01/10/1989';
+        blogpost.averageRating = 4;
+        blogpost.title = "Hello World";
+        blogpost.noOfShares = 100000;
+        blogpost.noOfViews = 1000000;
+        blogpost.isFlagged = true;
+        blogpost.lastUpdatedf = 'yesterday';
+        blogpost.content = "Hello, I'm Karen!!!!!!";
