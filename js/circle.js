06f7ac323bcc3e175e561f7609cb77f628b4d51a
karen-rivas0410 <karen-rivas0410@gmail.com>
2018-10-12 13:42:07 -0500

    added soluton to exercise

---
+++ js/circle.js
@@ -0,0 +1,36 @@
+(function() {
+    "use strict";
+
+// create a circle object
+var circle = {
+    radius: 3,
+    getArea: function () {
+        return Math.PI * Math.pow(this.radius,2); //alternative is (this.radius * this.radius)
+        // hint: area = pi * radius^2
+    },
+    logInfo: function (doRounding) {
+        if (doRounding === true) {
+            console.log(Math.round(this.getArea()));
+        } else {
+            console.log("Area of a circle with radius: " + this.radius + ", is: " + this.getArea());
+        }
+    }
+};
+    // log info about the circle
+    console.log("Raw circle information");
+    circle.logInfo(false);
+    console.log("Circle information rounded to the nearest whole number");
+    circle.logInfo(true);
+
+    console.log("=======================================================");
+    // TODO: Change the radius of the circle to 5.
+
+    circle.radius = 5;
+
+
+    // log info about the circle
+    console.log("Raw circle information");
+    circle.logInfo(false);
+    console.log("Circle information rounded to the nearest whole number");
+    circle.logInfo(true);
+})();
