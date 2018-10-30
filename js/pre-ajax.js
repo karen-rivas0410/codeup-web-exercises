db015cdeea49f085be1db44189af89fee28c7fdf
karen-rivas0410 <karen-rivas0410@gmail.com>
2018-10-30 08:51:14 -0500

    initial add js file for weather_map

--- js/pre-ajax.js
+++ js/pre-ajax.js
@@ -1,5 +1,5 @@
-"use strict";
 $(document).ready(function() {
+"use strict"; //when using this it has to be inside the document ready// it is function scope and would be better inside the ready document
 
     var people = [
         {
@@ -15,20 +15,47 @@ $(document).ready(function() {
             favColor: "yellow"
         }
     ];
+    //
+    // // $("div").children().hide();
+    //
+    // $("#add-names-btn").click(function() {
+    //         $("ul").html();
+    // });
+    //
+    //
+    //
+    // //could use a forEach loop IT IS MORE READABLE//or could use a each jquery functions//
+    //
+    // function adUlList(input) {
+    //     for (var i = 0; i >= input.length; i += 0) {
+    //         console.log(input[i].name);
+    //     }
+    //
+    // }
 
-    $("div").children().hide();
 
-    $("#add-names-btn").click(function() {
-        $("ul").slideToggle();
-    });
-
-
-
-    $("#name-list").html(function () {
-        for (var i = 0; i >= people.length; i += 0) {
-            $("").children().first().css("color", )
+    $.ajax({
+        url: "https://swapi.co/api/planets",
+        type: "GET",
+        data: {
+            search: "Yavin IV"
         }
+    }).done(function(data) {
+        console.log(data.results[0].diameter);
+    }).fail(function() {
+        console.log("Request failed");
     });
+
+
+
+    // $("#name-list").html(function () {
+    //     for (var i = 0; i >= people.length; i += 0) {
+    //         $("").children().first().css("color", )
+    //     }
+    // });
+
+
+
         // people.name.forEach(function(e) {
         //     console.log(e);
         //
